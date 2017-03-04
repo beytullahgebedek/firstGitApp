db171587bcf3380c4eb60eae3e39f7785b00608c
beytullahgebedek <beytullah.gebedek@gmail.com>
2017-03-04 22:48:45 +0300

    set config params for express

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname +'/public'));
 
-var ip = '127.0.0.1';
-var port = 3000;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 app.listen(port, ip);
